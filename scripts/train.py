beda3c3976b9115f0bfc8b03b1bcdafc5ab08bbb
DoniZefironi <s7ombra@gmail.com>
2026-01-30 18:48:43 +0300

    delete unused lib

--- scripts/train.py
+++ scripts/train.py
@@ -3,7 +3,6 @@ import numpy as np
 import ast
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.preprocessing import MultiLabelBinarizer
 import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 from pathlib import Path
